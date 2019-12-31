934857d1b24253a2d887fbca564b4c2ed0a87ea3
aserdar <serdarakkol@gmail.com>
2019-12-31 17:55:27 -0500

    Stopped for Spanish, no support of P2FA

--- Task/ForcedAligner_out.py
+++ Task/ForcedAligner_out.py
@@ -9,17 +9,15 @@ import os
 from pathlib import Path
 from p2fa import align
 
+txtdirname = r'/home/sakkol/Documents/Forced_Alignment/FORCE/Fast-English/txt'
+wavdirname = r'/home/sakkol/Documents/Forced_Alignment/FORCE/Fast-English/wav'
+TGdirname = r'/home/sakkol/Documents/Forced_Alignment/FORCE/Fast-English/TextGrid'
+matdirname = r'/home/sakkol/Documents/Forced_Alignment/FORCE/Fast-English/mat'
+
 txtdirname = r'/home/sakkol/Documents/Forced_Alignment/FORCE/Slow-English/txt'
-txtdirname = Path(txtdirname)
-
 wavdirname = r'/home/sakkol/Documents/Forced_Alignment/FORCE/Slow-English/wav'
-wavdirname = Path(wavdirname)
-
 TGdirname = r'/home/sakkol/Documents/Forced_Alignment/FORCE/Slow-English/TextGrid'
-TGdirname = Path(TGdirname)
-
 matdirname = r'/home/sakkol/Documents/Forced_Alignment/FORCE/Slow-English/mat'
-matdirname = Path(matdirname)
 
 # Define language here: Spanish or English
 lang = 'Spanish'
@@ -37,13 +35,62 @@ for names in items:
 
 # Iterate through text files, performing aligner and saving Praat as we go
 # for file in txtfilelist:
-for ff in range(1,len(txtfilelist)):
+for ff in range(0,len(txtfilelist)):
     
     curr_text = txtfilelist[ff]
     curr_wav = curr_text.replace('txt', "wav")
     curr_TextGird = curr_text.replace('txt', "TextGrid")
 
-    print(str(ff)+': '+curr_text)
+    print([str(ff)+': '+curr_text])
+    
+    phoneme_alignments, word_alignments = align.align(wavdirname+'/'+curr_wav, txtdirname+'/'+curr_text,TGdirname+'/'+curr_TextGird)
+            
+    with open(Path(matdirname,curr_text), 'w') as fp:
+        fp.writelines('Phonemes\n')
+        for pp in range(1,len(phoneme_alignments)):
+            fp.write(' '.join('%s' % x for x in phoneme_alignments[pp]))
+            fp.writelines('\n')
+        fp.writelines('Words\n')
+        for pp in range(1,len(word_alignments)):
+            fp.write(' '.join('%s' % x for x in word_alignments[pp]))
+            fp.writelines('\n')
+            
+
+
+## For Spanish (unfortunately P2FA doesn't have Spanish support)
+txtdirname = r'/home/sakkol/Documents/Forced_Alignment/FORCE/Fast-Spanish/txt'
+wavdirname = r'/home/sakkol/Documents/Forced_Alignment/FORCE/Fast-Spanish/wav'
+TGdirname = r'/home/sakkol/Documents/Forced_Alignment/FORCE/Fast-Spanish/TextGrid'
+matdirname = r'/home/sakkol/Documents/Forced_Alignment/FORCE/Fast-Spanish/mat'
+
+txtdirname = r'/home/sakkol/Documents/Forced_Alignment/FORCE/Slow-Spanish/txt'
+wavdirname = r'/home/sakkol/Documents/Forced_Alignment/FORCE/Slow-Spanish/wav'
+TGdirname = r'/home/sakkol/Documents/Forced_Alignment/FORCE/Slow-Spanish/TextGrid'
+matdirname = r'/home/sakkol/Documents/Forced_Alignment/FORCE/Slow-Spanish/mat'
+
+# Define language here: Spanish or English
+lang = 'Spanish'
+lang = 'English'
+
+# Get items in directory with text files here
+items = os.listdir(txtdirname)
+
+# Gather text file names from directory
+txtfilelist = []
+for names in items:
+    if names.endswith(".txt"):
+        txtfilelist.append(names)
+
+
+# Iterate through text files, performing aligner and saving Praat as we go
+# for file in txtfilelist:
+for ff in range(0,len(txtfilelist)):
+    
+    curr_text = txtfilelist[ff]
+    curr_wav = curr_text.replace('txt', "wav")
+    curr_TextGird = curr_text.replace('txt', "TextGrid")
+
+    print([str(ff)+': '+curr_text])
     
     phoneme_alignments, word_alignments = align.align(wavdirname+'/'+curr_wav, txtdirname+'/'+curr_text,TGdirname+'/'+curr_TextGird)
             
--- Task/ForcedAligner_prepare.m
+++ Task/ForcedAligner_prepare.m
@@ -1,3 +1,4 @@
+%% For English:
 % import excel to read sentences
 sentences = readtable('/home/sakkol/Documents/BACKUPS/Speech_Perception_BACKUP(2019.09.11)/SpeechPerception_Task(backup-11.09.2019)/SpeechPerception_Task/main_stim_loc/Sentence_to_use.xlsx');
 sentences = sentences.Sentence;
@@ -30,6 +31,45 @@ fclose(fid);
 % find and copy sentence wav -no dash
 wavname = [replace(sentences{s},' ','_') '-M.wav'];
 wavname_new = erase(wavname,'-M');
+copyfile(fullfile(wavdir,wavname),fullfile(newwavdir,wavname_new))
+
+end
+
+%% For Spanish:
+% import excel to read sentences
+sentences = readtable('/home/sakkol/Documents/BACKUPS/Spanish_SpeechPerception_Task_BACKUP_31.10.2019/main_stim_loc/sentence_to_use.xlsx');
+sentences = sentences.Sentence;
+
+% where texts are:
+txtdir = '/home/sakkol/Documents/BACKUPS/Spanish_SpeechPerception_Task_BACKUP_31.10.2019/Preparations/Version_4/All_Texts';
+
+wavdir = '/home/sakkol/Documents/BACKUPS/Spanish_SpeechPerception_Task_BACKUP_31.10.2019/main_stim_loc/Sentences_Rate0.9';
+newtxtdir = '/home/sakkol/Documents/Forced_Alignment/FORCE/Slow-Spanish/txt';
+newwavdir = '/home/sakkol/Documents/Forced_Alignment/FORCE/Slow-Spanish/wav';
+
+wavdir = '/home/sakkol/Documents/BACKUPS/Spanish_SpeechPerception_Task_BACKUP_31.10.2019/main_stim_loc/Sentences_Rate1.3';
+newtxtdir = '/home/sakkol/Documents/Forced_Alignment/FORCE/Fast-Spanish/txt';
+newwavdir = '/home/sakkol/Documents/Forced_Alignment/FORCE/Fast-Spanish/wav';
+
+
+
+% Loop for each sentence
+for s = 1:length(sentences)
+
+% find and copy sentence txt -no dash
+txtname = [replace(sentences{s},' ','_') '.txt'];
+fID = fopen(fullfile(txtdir,txtname), 'r','n','latin-1');
+textitself = fread(fID);
+fclose(fID);
+textitself = native2unicode(textitself, 'latin-1')';
+
+fid = fopen(fullfile(newtxtdir,txtname),'w');
+fprintf(fid,'%s',upper(textitself));
+fclose(fid);
+
+% find and copy sentence wav -no dash
+wavname = [replace(sentences{s},' ','_') '-F.wav'];
+wavname_new = erase(wavname,'-F');
 copyfile(fullfile(wavdir,wavname),fullfile(newwavdir,wavname_new))
 
 end
