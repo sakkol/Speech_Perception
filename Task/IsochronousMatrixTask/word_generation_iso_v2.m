a5ecfa03e4c99f5c836f2ee468812800b3fde829
aserdar <serdarakkol@gmail.com>
2020-11-18 16:09:59 -0500

    words are renewed

--- Task/IsochronousMatrixTask/word_generation_iso_v2.m
+++ Task/IsochronousMatrixTask/word_generation_iso_v2.m
@@ -12,6 +12,7 @@ for y=1:7
     fclose(fid);
 end
 
+% once they are created
 sound_dir = '/home/sakkol/Documents/TASKS/PREPARATIONS/IsochronousMatrixTask/vol2/word_sounds/English';
 
 for y=1:7
@@ -19,14 +20,14 @@ for y=1:7
     
     [soundtocare,SampleRate] = audioread(fullfile(sound_dir,[curr_sent '-M.wav']));
     
-    
-    thr_ampl = 0.001;
-    refract_tpts = floor(0.3*SampleRate); % 1 second only
+    thr_ampl = 0.0005;
+    refract_tpts = floor(0.33*SampleRate); % 1 second only
     digital_trig_chan=analog2digital_trig(soundtocare',thr_ampl,refract_tpts,0);
-    trial_onsets_tpts=find(digital_trig_chan==1);trial_onsets_tpts=trial_onsets_tpts-500;
+    trial_onsets_tpts=find(digital_trig_chan==1);trial_onsets_tpts=trial_onsets_tpts-2000;trial_onsets_tpts(trial_onsets_tpts<0)=1;
     
     digital_trig_chan=analog2digital_trig(soundtocare(end:-1:1)',thr_ampl,refract_tpts,0);
-    trial_ends_tpts=find(digital_trig_chan==1);trial_ends_tpts = trial_ends_tpts + 500;
+    trial_ends_tpts=find(digital_trig_chan==1);trial_ends_tpts = length(soundtocare) - trial_ends_tpts;
+    trial_ends_tpts = trial_ends_tpts + 2000;trial_ends_tpts = trial_ends_tpts(end:-1:1);
     
     figure('Units','normalized','Position', [0 0  1 .5]);
     plot(1/SampleRate:1/SampleRate:(length(soundtocare)/SampleRate),soundtocare); hold on
@@ -48,6 +49,9 @@ for y=1:7
         curr_word = sentence_table{y,w}{1};
         for_crr = soundtocare(trial_onsets_tpts(w):trial_ends_tpts(w));
         for_crr = for_crr(find(for_crr,1,'first'):find(for_crr,1,'last'));
+        verylows = for_crr<0.0014 & for_crr>-0.0014;
+        for_crr = for_crr(find(~verylows,1,'first'):find(~verylows,1,'last'));
+        for_crr = for_crr/max(abs(for_crr));
         audiowrite(fullfile(sound_dir,[curr_word '-M.wav']),for_crr,SampleRate);
     end
 end
@@ -56,45 +60,16 @@ end
 
 
 
-
-
-
-%% Several words need extra care
-[soundtocare,SampleRate] = audioread('/home/sakkol/Documents/TASKS/PREPARATIONS/IsochronousMatrixTask/vol1/word_sounds/English/Jane, got, Sue, to, buy, Sean, two, cheap, cups-M.wav');
-
-sound_dir = '/home/sakkol/Documents/TASKS/PREPARATIONS/IsochronousMatrixTask/vol1/word_sounds/English';
-
-audiowrite(fullfile(sound_dir,'Jane-M.wav'),soundtocare(923:12921),SampleRate);
-audiowrite(fullfile(sound_dir,'Sue-M.wav'),soundtocare(30700:42475),SampleRate);
-audiowrite(fullfile(sound_dir,'Sean-M.wav'),soundtocare(73031:86086),SampleRate);
-audiowrite(fullfile(sound_dir,'cheap-M.wav'),soundtocare(103587:113300),SampleRate);
-audiowrite(fullfile(sound_dir,'cups-M.wav'),soundtocare(115500:128074),SampleRate);
-
-% for now and kept
-[soundtocare,SampleRate] = audioread('/home/sakkol/Documents/TASKS/PREPARATIONS/IsochronousMatrixTask/vol1/word_sounds/English/I, kept, now-M.wav');
-
-sound_dir = '/home/sakkol/Documents/TASKS/PREPARATIONS/IsochronousMatrixTask/vol1/word_sounds/English';
-
-audiowrite(fullfile(sound_dir,'kept-M.wav'),soundtocare(8851:19190),SampleRate);
-audiowrite(fullfile(sound_dir,'now-M.wav'),soundtocare(23323:36000),SampleRate);
-
 %% After generating sounds through GoogleCloud_TTS_iso.py, change the sound volume
 
-sound_dir = '/home/sakkol/Documents/TASKS/PREPARATIONS/IsochronousMatrixTask/vol1/word_sounds/English';
-save_dir = fullfile(sound_dir,'word_spectrograms');
+sound_dir = '/home/sakkol/Documents/TASKS/PREPARATIONS/IsochronousMatrixTask/vol2/word_sounds/English';
+save_dir = fullfile(sound_dir,'word_spectrograms');mkdir(save_dir)
 soundss = dir([sound_dir '/*.wav']);
 
 WordsInfo=table;
 for s = 1:length(soundss)
     filename = fullfile(soundss(s).folder,soundss(s).name);
-    [curr_sound,Fs] = audioread(filename);
-    
-    % normalize to its max
-    new_sound = curr_sound/max(abs(curr_sound));
-    verylows = new_sound<0.005 & new_sound>-0.005;
-%     new_sound = new_sound(find(new_sound,1,'first'):find(new_sound,1,'last'));
-    new_sound = new_sound(find(~verylows,1,'first'):find(~verylows,1,'last'));
-    audiowrite(fullfile(soundss(s).folder,soundss(s).name),new_sound,Fs);
+    [new_sound,Fs] = audioread(filename);
     
     % save the spectrogram plot
     WordsInfo.StimName{s} = erase(soundss(s).name,'-M.wav');
@@ -121,7 +96,7 @@ end
 
 % Estimating the K value, to multiply with noise for SNR
 Signal_Power=[];Noise_Power=[];K=[];
-for s=[22,9,31,35]
+for s=[25,10,39,35]  % now, catch, these, words
     % generate noise
     cn = dsp.ColoredNoise('Color','pink','SamplesPerFrame',44100,'NumChannels',1);
     curr_noise = cn();clear cn;curr_noise=[curr_noise;curr_noise;curr_noise;curr_noise;curr_noise];
@@ -147,36 +122,86 @@ Noise_Power=Noise_Power(Noise_Power~=0);
 avg_sig_pow = mean(Signal_Power);
 avg_noise_pow = mean(Noise_Power);
 
-words_table = readtable('/home/sakkol/Documents/TASKS/PREPARATIONS/IsochronousMatrixTask/vol1/English_single_words.xlsx');
+words_table = readtable('/home/sakkol/Documents/TASKS/PREPARATIONS/IsochronousMatrixTask/vol2/English_single_words.xlsx');
 
 % save several things here
 save(fullfile(save_dir,'EnglishWordsInfo.mat'),'WordsInfo','avg_sig_pow','avg_noise_pow','words_table')
 
 %% Words in adaptation and threshold
-load('default_conditions.mat');
+def=load('default_conditions.mat');
+
+% prepare adaptation and threshold word list
+[events_table] = events_wrapper([],'English','both',[]) % choose with selections_GUI
+cfgs_for_adapt_English = events_table.cfgs;
 
 adaptation_word_list_English = cell(10,20);
-for w = 1:10
+for t = 1:10
     for p=1:5
         for w=1:4
-            adaptation_word_list_English{w,(4*(p-1))+w} = cfgs_for_adapt_English{w}.(['part' num2str(p+1)]).(['word' num2str(w)]){:};
+            adaptation_word_list_English{t,(4*(p-1))+w} = cfgs_for_adapt_English{t}.(['part' num2str(p+1)]).(['word' num2str(w)]){:};
         end
     end
 end
 
+[events_table] = events_wrapper([],'English','both',[]) % choose with selections_GUI
+cfgs_for_thresh_English = events_table.cfgs;
+
+for t = 1:30
+    if mod(t,3)==0
+        cfgs_for_thresh_English{t}.SNR = -6;
+    elseif mod(t,3)==1
+        cfgs_for_thresh_English{t}.SNR = -2;
+    elseif mod(t,3)==2
+        cfgs_for_thresh_English{t}.SNR = 2;
+    end
+end
+cfgs_for_thresh_English = cfgs_for_thresh_English(randperm(size(cfgs_for_thresh_English,1)));
 threshold_word_list_English = cell(30,5);
-for w = 1:30
-    threshold_word_list_English{w,1} = cfgs_for_thresh_English{w}.SNR;
+for t = 1:30
+    threshold_word_list_English{t,1} = cfgs_for_thresh_English{t}.SNR;
     for p=1
         for w=1:4
             
-            threshold_word_list_English{w,(4*(p-1))+w+1} = cfgs_for_thresh_English{w}.(['part' num2str(p+2)]).(['word' num2str(w)]){:};
+            threshold_word_list_English{t,(4*(p-1))+w+1} = cfgs_for_thresh_English{t}.(['part' num2str(p+2)]).(['word' num2str(w)]){:};
         end
     end
 end
 
+% now get the defaults again
+iso_mat_24 = def.iso_mat_24;
+FreeRecall_3sent = def.FreeRecall_3sent; % changing to 1.5Hz
+def.FreeRecall_3sent.Frequency(1) = 1.5;
+def.FreeRecall_3sent.Frequency(2) = 1.5;
+adapt_thresh = def.adapt_thresh;
+clear_table = def.clear_table;
+passive_5sent = def.passive_5sent;
+
+% Create all stimuli to select from for Free Recall
+events_table1 = events_wrapper([],'English','both','FreeRecall_3sent'); % only sentence, 105 trials
+events_table2 = events_wrapper([],'English','both','FreeRecall_3sent'); % only scrambled, 105 trials
+words_list = cell(210,12);
+for t=1:105
+    words_list(t,:) = [events_table1.trial_words{t}];
+end
+for t=1:105
+    words_list(t+105,:) = [events_table2.trial_words{t}];
+end
+events_table = [events_table1;events_table2];
+cfgs_for_FreeRecall_English = table2cell(events_table(:,2:4));
+
 save('default_conditions.mat','iso_mat_24','passive_5sent','FreeRecall_3sent','clear_table','adapt_thresh',...
-    'cfgs_for_adapt_English','cfgs_for_thresh_English','adaptation_word_list_English','threshold_word_list_English')
+    'cfgs_for_adapt_English','cfgs_for_thresh_English','cfgs_for_FreeRecall_English','words_list','adaptation_word_list_English','threshold_word_list_English')
 
 
+%% Create example stimuli
+sound_dir = '/home/sakkol/Documents/TASKS/PREPARATIONS/IsochronousMatrixTask/vol2/word_sounds/English';
+save_dir = fullfile(sound_dir,'example_stim');mkdir(save_dir)
+
+events_table = events_wrapper([],'English','both','FreeRecall_3sent');
+
+for t=1:30
+    audiowrite(fullfile(save_dir,[num2str(t) '-' events_table.cond_info{t}.Number_of_sentences{1} '-' events_table.cond_info{t}.Sentence_vs_Scrambled{1} '-.wav']),...
+        events_table.trials{t},44100)
+end
+    
 
